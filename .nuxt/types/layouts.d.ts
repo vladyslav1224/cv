d42eb5ffea2fcbd6e03e2e3ab5b9aeaf5429575f
vlxdsxd <v.pelykh@devocean.pro>
2024-01-28 21:26:23 +0200

    update npm i

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,6 +1,6 @@
 import { ComputedRef, Ref } from 'vue'
 export type LayoutKey = "default" | "page" | "section"
-declare module "/Users/vlxdsxd/Desktop/vlxd project/cv/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "C:/Users/vlxdsxd/Documents/GitHub/cv/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
   }
--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = string
-declare module "/Users/vlxdsxd/Desktop/vlxd project/cv/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "C:/Users/vlxdsxd/Documents/GitHub/cv/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
